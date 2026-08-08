b5488e0ba6f3902b4407411e003de4ba40cf462a
AmirHossein Zarei <Amirhosseinzarei.org@gmail.com>
2026-08-09 01:01:52 +0330

    Change TProxyStopService to return a Boolean

--- app/src/main/java/hev/htproxy/TProxyService.kt
+++ app/src/main/java/hev/htproxy/TProxyService.kt
@@ -2,6 +2,6 @@ package hev.htproxy
 
 object TProxyService {
     @JvmStatic external fun TProxyStartService(configPath: String, fd: Int): Boolean
-    @JvmStatic external fun TProxyStopService()
+    @JvmStatic external fun TProxyStopService(): Boolean
     @JvmStatic external fun TProxyGetStats(): LongArray
 }
